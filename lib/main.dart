234833362c16fddc6c4e65712a35fa675b553a13
AhmedAbogameel <gamilco81@gmail.com>
2022-08-25 16:10:51 +0200

    Merge Codes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:soheel_app/views/shared/splash/view.dart';
 import 'package:soheel_app/widgets/pop_scaffold.dart';
@@ -8,6 +9,7 @@ import 'core/router/router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   DioHelper.init();
   await AppStorage.init();
   // getUserAndCache(258, 1);
