63034b3ebd49c65f59fda7a9d5f24c7f69e2974e
AhmedAbogameel <gamilco81@gmail.com>
2022-09-16 14:09:17 +0200

    ChangePasswordBug

--- lib/views/shared/auth/create_new_password/cubit/cubit.dart
+++ lib/views/shared/auth/create_new_password/cubit/cubit.dart
@@ -2,13 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:soheel_app/views/shared/auth/create_new_password/cubit/states.dart';
-import 'package:soheel_app/views/shared/auth/login/cubit/states.dart';
-import 'package:soheel_app/views/shared/auth/otp/view.dart';
-import 'package:soheel_app/views/shared/auth/signup/cubit/states.dart';
-import 'package:soheel_app/views/shared/trips/view.dart';
-import 'package:soheel_app/views/user/home/view.dart';
 import 'package:soheel_app/widgets/snack_bar.dart';
-
 import '../../../../../core/app_storage/app_storage.dart';
 import '../../../../../core/dio_manager/dio_manager.dart';
 import '../../../../../core/router/router.dart';
@@ -25,20 +19,25 @@ class NewPasswordCubit extends Cubit<NewPasswordStates> {
     formKey.currentState!.save();
     if (!formKey.currentState!.validate()) return;
     emit(NewPasswordLoadingState());
-    try{
-        final response = await DioHelper.post(
-            'user/account/edit_password',
-            data: {
-              "logged": true,
-              "customer_id" :AppStorage.customerID,
-              "password" : password,
-              "confirm" : confirm,
-            });
-        final data = response.data;
+    try {
+      final response = await DioHelper.post(
+        'user/account/edit_password',
+        data: {
+          "logged": true,
+          "customer_id": AppStorage.customerID,
+          "password": password,
+          "confirm": confirm,
+        },
+      );
+      final data = response.data;
+      if (data['success'] != null) {
         showSnackBar(data['success']);
-
-    }catch(e){
-      emit(NewPasswordErrorState(e.toString()));
+        RouteManager.pop();
+      } else {
+        showSnackBar(data.toString());
+      }
+    } catch (e) {
+      showSnackBar(e.toString());
     }
     emit(NewPasswordInitState());
   }
